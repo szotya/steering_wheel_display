6dbe1755fb50b7bb27c0920d338cff40ad95b89f
Kristóf Kékesi <64812024+KristofKekesi@users.noreply.github.com>
2024-10-15 10:57:05 +0000

    Feature: skeleton teszt üzenetek kicsomagolásának az idejéhez

---
+++ modules/stopper.py
@@ -0,0 +1,10 @@
+"""
+Stopper modul megadott callback függvény lefutási idejének méréséhez.
+A visszatérített érték miliszekundumban van.
+"""
+async def stopper(callback) -> float:
+    start = time.perf_counter()
+    await callback()
+    end = time.perf_counter()
+    
+    return (end - start) * 1000  # Return time in milliseconds
---
+++ tests/test_unpack_speed.py
@@ -0,0 +1,17 @@
+import unittest
+import time
+from modules.stopper import stopper
+
+class TestUnpackSpeed(unittest.TestCase):
+    """
+    Csomagok kicsomagolásának idejét mérő teszt. Sikeres, ha adott időn belül csomagol ki egy üzenetet.
+    """
+
+     def test(self):
+        target: float = 1.5 # milisec
+
+        def wrapper() -> None:
+            time.sleep(1)
+
+        delta: float = stopper()
+        self.assertTrue(delta <= float)
