2f39a7359e8d3784986ecc0eb451b20a5772bf6a
Kristóf Kékesi <64812024+KristofKekesi@users.noreply.github.com>
2024-10-15 10:58:47 +0000

    Fix: unexpected indent

--- tests/test_unpack_speed.py
+++ tests/test_unpack_speed.py
@@ -7,7 +7,7 @@ class TestUnpackSpeed(unittest.TestCase):
     Csomagok kicsomagolásának idejét mérő teszt. Sikeres, ha adott időn belül csomagol ki egy üzenetet.
     """
 
-     def test(self):
+    def test(self):
         target: float = 1.5 # milisec
 
         def wrapper() -> None:
